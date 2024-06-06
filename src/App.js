0ec10677a6266c38e14c5ea71096f9f2e61acf5a
Eli Gutovsky <eli_g@optimove.com>
2024-06-06 21:12:42 +0300

    chore: Update HTML and CSS for pirate website login page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
 function App() {
+  const code = '2562';
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (inputValue !== code) {
+      alert('Неверный код');
+      return;
+    }
+    alert('АААААА! Вы взломали пиратский сайт!');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Пиратский сайт</h1>
+        <div className="chest-container">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Код"
+          />
+          <button onClick={handleSubmit}>Войти</button>
+        </div>
       </header>
     </div>
   );
